6f14cca953b0aabe46d2e434354a69267a064506
Sanidhya Sahu <115490812+isanidhya@users.noreply.github.com>
2023-08-05 15:26:03 +0530

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,20 @@
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String street;
+    private String address;
+    private String city;
+    private String state;
+    private String email;
+    private String phone;
+
+    // Getters and Setters
+
+    // Constructors
+
+    // Additional methods (if required)
+}
